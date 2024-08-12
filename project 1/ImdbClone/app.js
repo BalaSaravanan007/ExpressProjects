6fdbd560142f47038d2b9774ad057ff39933fbfe
BalaSaravanan007 <jbalasaravanan007@gmail.com>
2024-08-12 11:16:09 +0530

    no longer available

--- project 1/ImdbClone/app.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const movieRouter = require("./router/movieRouter");
-const app = express();
-
-app.use("/movies", movieRouter);
-
-module.exports = app;
--- project 1/ImdbClone/router/movieRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const movieController = require("./../Controller/movieController");
-
-router.route("/").get(movieController.GetAllMovies);
-
-module.exports = router;
